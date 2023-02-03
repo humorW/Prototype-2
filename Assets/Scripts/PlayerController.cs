e22e71e00f30c5fd329df8e6bffffc80677fa36a
humorW <121972279+humorW@users.noreply.github.com>
2023-02-03 15:44:34 -0800

    2.3 new

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 10.0f;
+   
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -17,23 +18,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (transform.position.x < -xRange)
         {
-            // Launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform. rotation);
-        }
-    
-        // keep the player in bounds
-        if(transform.position.x < -xRange) 
-        {
-            transform.position = new Vector3(-xRange,transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         } 
-        if(transform.position.x > xRange)
+        
+
+         if (transform.position.x < xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizont");
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        
+        if (Input.GetkeyDown(KeyCode.Space)) 
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }   
     }
     
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -18,13 +18,8 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        if (Input.GetkeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length)
-                Vector3 spwnPos = new Vector3(Random.Range(-spawnRangeX, spwnRageX), 0, spawnPosZ);
-
-            Instantiate(animalPerfabs[animalIndex],spawnPos, animalPerfabs[animalIndex].transform.rotation);
-        }
+        
+        
     }
     
 }
